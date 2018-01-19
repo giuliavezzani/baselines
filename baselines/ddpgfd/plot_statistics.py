1c01946cc3d5c21e2148434576dfb9bd3aedb3da
Giulia Vezzani <giuliavezzani@vzzgli@gmail.com>
2018-01-19 09:48:04 -0800

    added module for plotting the results

---
+++ baselines/ddpgfd/plot_statistics.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+from baselines import logger
+import pandas
+import argparse
+import numpy as np
+from matplotlib.pyplot import cm
+
+
+
+
+def plot(file_path, **kwargs):
+    data = logger.read_csv(file_path)
+    plt.subplots_adjust(wspace=0.5, hspace=0.5)
+    count = 1
+    color=cm.rainbow(np.linspace(0,1,26))
+
+    for key in sorted(data.keys()):
+        x = np.arange(0,len(data[key]))
+        y = data[key]
+        print(count)
+        c = color[count - 1]
+        plt.subplot(5,6, count)
+        plt.xlabel('Epochs ', fontsize=8)
+        plt.ylabel(key, fontsize=8)
+        plt.plot(x,y, linewidth=2, c=c)
+        count += 1
+
+    plt.show()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--file-path', type=str, default='progress.csv')
+    args = parser.parse_args()
+    # we don't directly specify timesteps for this script, so make sure that if we do specify them
+    # they agree with the other parameter
+    dict_args = vars(args)
+    return dict_args
+
+if __name__ == "__main__":
+    args = parse_args()
+    plot(**args)
