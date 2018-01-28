087823609d470ec92bfde4c4c33205e7e0fd7764
Giulia Vezzani <giuliavezzani@vzzgli@gmail.com>
2018-01-27 18:45:34 -0800

    new file for running DDPG in a different way

---
+++ baselines/ddpgfd/training_pre_train.py
@@ -0,0 +1,235 @@
+import os
+import time
+from collections import deque
+import pickle
+
+from baselines.ddpgfd.ddpgfd import DDPGFD
+from baselines.ddpgfd.util import mpi_mean, mpi_std, mpi_max, mpi_sum
+import baselines.common.tf_util as U
+
+from baselines import logger
+import numpy as np
+import tensorflow as tf
+from mpi4py import MPI
+
+
+def train(env, nb_epochs, nb_epoch_cycles, render_eval, reward_scale, render, param_noise, actor, critic,
+    normalize_returns, normalize_observations, critic_l2_reg, actor_lr, critic_lr, action_noise, saving_folder,
+    popart, gamma, clip_norm, nb_train_steps, nb_rollout_steps, nb_eval_steps, batch_size, batch_size_bc,priorization_off, nstep_loss_off,
+    memory, eps, eps_d, lambda_3, lambda_n, alpha,  target_period_update, nb_training_bc,t_inner_steps,n_value, behaviour_cloning_off,
+    tau=0.01, eval_env=None, param_noise_adaption_interval=50):
+    rank = MPI.COMM_WORLD.Get_rank()
+
+    assert (np.abs(env.action_space.low) == env.action_space.high).all()  # we assume symmetric actions.
+    max_action = env.action_space.high
+    logger.info('scaling actions by {} before executing in env'.format(max_action))
+    agent = DDPGFD(actor, critic, memory, env.observation_space.shape, env.action_space.shape, eps, eps_d,
+        lambda_3, priorization_off=priorization_off,nstep_loss_off=nstep_loss_off, lambda_n=lambda_n, alpha=alpha, n_value=n_value,gamma=gamma, tau=tau, normalize_returns=normalize_returns,
+        normalize_observations=normalize_observations,batch_size=batch_size, batch_size_bc=batch_size_bc,
+        t_inner_steps=t_inner_steps, action_noise=action_noise, param_noise=param_noise, critic_l2_reg=critic_l2_reg,
+        actor_lr=actor_lr, critic_lr=critic_lr, enable_popart=popart, clip_norm=clip_norm, reward_scale=reward_scale)
+    logger.info('Using agent with the following configuration:')
+    logger.info(str(agent.__dict__.items()))
+
+    # Set up logging stuff only for a single worker.
+    if rank == 0:
+        saver = tf.train.Saver()
+    else:
+        saver = None
+
+    step = 0
+    episode = 0
+    eval_episode_rewards_history = deque(maxlen=100)
+    episode_rewards_history = deque(maxlen=100)
+    with U.single_threaded_session() as sess:
+        # Prepare everything.
+        agent.initialize(sess)
+        sess.graph.finalize()
+
+        if not behaviour_cloning_off:
+            for t_train_bc in range(nb_training_bc):
+                #print('---- bc step -----', t_train_bc)
+                agent.behaviour_cloning()
+
+        agent.reset()
+        obs = env.reset()
+        if eval_env is not None:
+            eval_obs = eval_env.reset()
+        done = False
+        episode_reward = 0.
+        episode_step = 0
+        episodes = 0
+        t = 0
+
+        epoch = 0
+        start_time = time.time()
+
+        epoch_episode_rewards = []
+        epoch_episode_steps = []
+        epoch_episode_eval_rewards = []
+        epoch_episode_eval_steps = []
+        epoch_start_time = time.time()
+        epoch_actions = []
+        epoch_qs = []
+        epoch_episodes = 0
+
+        for epoch in range(nb_epochs):
+            for cycle in range(nb_epoch_cycles):
+                # Collect more rollouts
+                rollout_acts = []
+                rollout_obs0 = []
+                rollout_obs1 = []
+                rollout_rews = []
+                rollout_terms1 = []
+                print('num epoch', epoch)
+                print('num cycle', cycle)
+                if  epoch == 0  and  cycle == 0:
+                    pass
+                else:
+                    for t_rollout in range(nb_rollout_steps):
+                    # Predict next action.
+
+                        print('num roll', t_rollout)
+                        start_time = time.time()
+
+                        #print('num rollout collected: ', t_rollout)
+
+                        if done:
+                            obs = env.reset()
+                            agent.reset()
+                            done = False
+                        while not done:
+
+                            rollout_obs0.append(obs)
+                            action, q = agent.pi(obs, apply_noise=True, compute_Q=True)
+                            assert action.shape == env.action_space.shape
+
+                            # Execute next action.
+                            if rank == 0 and render:
+                                env.render()
+                            assert max_action.shape == action.shape
+
+                            new_obs, r, done, info = env.step(max_action * action)  # scale for execution in env (as far as DDPGfD is concerned, every action is in [-1, 1])
+                            t += 1
+
+                            if rank == 0 and render and epoch/nb_epochs >= 0.8:
+                                env.render()
+                            episode_reward += r
+                            episode_step += 1
+
+                            # Book-keeping.
+                            epoch_actions.append(action)
+                            epoch_qs.append(q)
+                            rollout_obs1.append(new_obs)
+                            rollout_acts.append(action)
+                            rollout_terms1.append(done)
+                            rollout_rews.append(r)
+
+                            obs = new_obs
+
+                        print('time for collecting rollout', time.time() - start_time)
+
+                        start_time = time.time()
+                        epoch_episode_rewards.append(episode_reward)
+                        episode_rewards_history.append(episode_reward)
+                        epoch_episode_steps.append(episode_step)
+                        episode_reward = 0.
+                        episode_step = 0
+                        epoch_episodes += 1
+                        episodes += 1
+
+                    # Train.
+                        agent.store_transition(rollout_obs0, rollout_acts, rollout_rews, rollout_obs1, rollout_terms1)
+                epoch_actor_losses = []
+                epoch_critic_losses = []
+                epoch_adaptive_distances = []
+
+                for t_train in range(nb_train_steps):
+                    print('num_trein', t_train)
+                    # Adapt param noise, if necessary.
+                    if memory.nb_entries >= batch_size and t % param_noise_adaption_interval == 0:
+                        distance = agent.adapt_param_noise()
+                        epoch_adaptive_distances.append(distance)
+
+                    cl, al = agent.train(t_train)
+                    epoch_critic_losses.append(cl)
+                    epoch_actor_losses.append(al)
+                    if (np.mod(t_train,target_period_update) == 0 ):
+                        agent.update_target_net()
+
+                # Evaluate.
+                eval_episode_rewards = []
+                eval_qs = []
+                if eval_env is not None:
+                    eval_episode_reward = 0.
+                    for t_rollout in range(nb_eval_steps):
+                        eval_action, eval_q = agent.pi(eval_obs, apply_noise=False, compute_Q=True)
+                        eval_obs, eval_r, eval_done, eval_info = eval_env.step(max_action * eval_action)  # scale for execution in env (as far as DDPGfd is concerned, every action is in [-1, 1])
+                        if render_eval:
+                            eval_env.render()
+                        eval_episode_reward += eval_r
+
+                        eval_qs.append(eval_q)
+                        if eval_done:
+                            eval_obs = eval_env.reset()
+                            eval_episode_rewards.append(eval_episode_reward)
+                            eval_episode_rewards_history.append(eval_episode_reward)
+                            eval_episode_reward = 0.
+
+            current_time = time.localtime()
+
+            if np.mod(epoch, 10) == 0:
+                var = tf.trainable_variables()
+                var_value = {}
+                for v in var:
+                    var_value[v.name] = sess.run(v)
+                pickle.dump(var_value, open(saving_folder +'/trained-variables-DDPGfD-''-'+time.strftime('%Y-%m-%d-%H-%M-%S', current_time)+'-'+str(epoch)+'.pkl', 'wb'))
+
+            # Log stats.
+            epoch_train_duration = time.time() - epoch_start_time
+            duration = time.time() - start_time
+            stats = agent.get_stats()
+            combined_stats = {}
+            for key in sorted(stats.keys()):
+                combined_stats[key] = mpi_mean(stats[key])
+
+            # Rollout statistics.
+            combined_stats['rollout/return'] = mpi_mean(epoch_episode_rewards)
+            combined_stats['rollout/return_history'] = mpi_mean(np.mean(episode_rewards_history))
+            combined_stats['rollout/episode_steps'] = mpi_mean(epoch_episode_steps)
+            combined_stats['rollout/episodes'] = mpi_sum(epoch_episodes)
+            combined_stats['rollout/actions_mean'] = mpi_mean(epoch_actions)
+            combined_stats['rollout/actions_std'] = mpi_std(epoch_actions)
+            combined_stats['rollout/Q_mean'] = mpi_mean(epoch_qs)
+
+            # Train statistics.
+            combined_stats['train/loss_actor'] = mpi_mean(epoch_actor_losses)
+            combined_stats['train/loss_critic'] = mpi_mean(epoch_critic_losses)
+            combined_stats['train/param_noise_distance'] = mpi_mean(epoch_adaptive_distances)
+
+            # Evaluation statistics.
+            if eval_env is not None:
+                combined_stats['eval/return'] = mpi_mean(eval_episode_rewards)
+                combined_stats['eval/return_history'] = mpi_mean(np.mean(eval_episode_rewards_history))
+                combined_stats['eval/Q'] = mpi_mean(eval_qs)
+                combined_stats['eval/episodes'] = mpi_mean(len(eval_episode_rewards))
+
+            # Total statistics.
+            combined_stats['total/duration'] = mpi_mean(duration)
+            combined_stats['total/steps_per_second'] = mpi_mean(float(t) / float(duration))
+            combined_stats['total/episodes'] = mpi_mean(episodes)
+            combined_stats['total/epochs'] = epoch + 1
+            combined_stats['total/steps'] = t
+
+            for key in sorted(combined_stats.keys()):
+                logger.record_tabular(key, combined_stats[key])
+            logger.dump_tabular()
+            logger.info('')
+            logdir = logger.get_dir()
+            if rank == 0 and logdir:
+                if hasattr(env, 'get_state'):
+                    with open(os.path.join(logdir, 'env_state.pkl'), 'wb') as f:
+                        pickle.dump(env.get_state(), f)
+                if eval_env and hasattr(eval_env, 'get_state'):
+                    with open(os.path.join(logdir, 'eval_env_state.pkl'), 'wb') as f:
+                        pickle.dump(eval_env.get_state(), f)
